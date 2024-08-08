135aad78f29db22666e5637b637989bb0df5ab80
bidstrup <01bidstrup@gmail.com>
2024-08-08 19:53:44 +0200

    remove interaction tracking when closing inventory

---
+++ src/main/java/dev/aucorg/armourstandmodeler/eventlistener/InventoryCloseEventListener.java
@@ -0,0 +1,18 @@
+package dev.aucorg.armourstandmodeler.eventlistener;
+
+import dev.aucorg.armourstandmodeler.ArmourStandInteractionMap;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+
+public class InventoryCloseEventListener implements Listener {
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent event) {
+        Player player = (Player) event.getPlayer();
+
+        if (ArmourStandInteractionMap.isPlayerInteracting(player)) {
+            ArmourStandInteractionMap.removeInteraction(player);
+        }
+    }
+}
