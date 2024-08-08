03ef9b77e3edd23568cb1edc51192465c847018d
bidstrup <01bidstrup@gmail.com>
2024-08-08 16:22:31 +0200

    setup interact event listener

--- src/main/java/dev/aucorg/armourstandmodeler/ArmourStandInteractEventListener.java
+++ src/main/java/dev/aucorg/armourstandmodeler/ArmourStandInteractEventListener.java
@@ -6,7 +6,6 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class ArmourStandInteractEventListener implements Listener {
     @EventHandler
